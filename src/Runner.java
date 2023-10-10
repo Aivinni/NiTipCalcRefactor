cd35ad84ea619e203602d19e16a8ad5561b3eaad
aivin1 <aivinni1219@gmail.com>
2023-10-09 23:10:18 -0400

    final version

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Runner {
+import java.util.Scanner;
+public class Runner {
+    public static void main(String[] args) {
+        String name;
+        int numInGroup;
+        String country;
+        int percentage;
+        String item;
+        double cost;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to Tip Diner!");
+        System.out.println("What is your name? ");
+        name = scan.nextLine();
+        System.out.println("Hello, " + name);
+        System.out.println("How many people are in your group? ");
+        numInGroup = scan.nextInt();
+        scan.nextLine();
+        System.out.println("What country are you in? ");
+        country = scan.nextLine().toLowerCase();
+        System.out.println("What is the tip percentage? (If tipping not required put 0) ");
+        percentage = scan.nextInt();
+        scan.nextLine();
+
+        TipCalculator bill1 = new TipCalculator(numInGroup, percentage, country);
+
+        boolean run = true;
+        while (run) {
+            while (run) {
+                System.out.println("Enter your item (-1 to stop, -1 will not be recorded) ");
+                item = scan.nextLine();
+                if (!item.equals("-1")) {
+                    System.out.println("Enter cost of item ");
+                    cost = scan.nextDouble();
+                    scan.nextLine();
+                    bill1.addMeal(item, cost);
+                }
+                if (item.equals("-1")) {
+                    run = false;
+                }
+            }
+        }
+        System.out.println(bill1.printReceipt());
+    }
 }
--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,83 @@
-package PACKAGE_NAME;public class TipCalculator {
+public class TipCalculator {
+    private final int numPeople;
+    private final int tipPercentage;
+    private double totalBillBeforeTip;
+    private final String country;
+    private String items;
+
+    private double round(double input) {
+        input *= 100;
+        input = (int)input;
+        input /= 100.0;
+        return input;
+    }
+    public TipCalculator(int numPeople, int tipPercentage, String country) {
+        this.numPeople = numPeople;
+        this.tipPercentage = tipPercentage;
+        totalBillBeforeTip = 0.0;
+        this.country = country;
+    }
+    public double getTotalBillBeforeTip() {
+        return totalBillBeforeTip;
+    }
+    public int getTipPercentage() {
+        return tipPercentage;
+    }
+    public void addMeal(String item, double cost) {
+        items += item + " ";
+        totalBillBeforeTip += cost;
+        totalBillBeforeTip = round(totalBillBeforeTip);
+    }
+    public double tipAmount() {
+        return round(((double)tipPercentage / 100) * totalBillBeforeTip);
+    }
+    public double totalBill() {
+        return round(tipAmount() + totalBillBeforeTip);
+    }
+    public double perPersonCostBeforeTip() {
+        return round(totalBillBeforeTip / numPeople);
+    }
+    public double perPersonTipAmount() {
+        return round(tipAmount() / numPeople);
+    }
+    public double perPersonTotalCost() {
+        return round(totalBill() / numPeople);
+    }
+    public boolean countryWithTip() {
+        return !country.equalsIgnoreCase("Japan") &&
+                !country.equalsIgnoreCase("China") &&
+                !country.equalsIgnoreCase("French Polynesia") &&
+                !country.equalsIgnoreCase("Korea") &&
+                !country.equalsIgnoreCase("Hong Kong") &&
+                !country.equalsIgnoreCase("Switzerland") &&
+                !country.equalsIgnoreCase("Australia") &&
+                !country.equalsIgnoreCase("Belgium") &&
+                !country.equalsIgnoreCase("Brazil") &&
+                !country.equalsIgnoreCase("Denmark") &&
+                !country.equalsIgnoreCase("Estonia") &&
+                !country.equalsIgnoreCase("New Zealand");
+    }
+    public String printReceipt() {
+        int i;
+        String letter;
+        StringBuilder itemToPrint = new StringBuilder();
+        int indexPrevSpace = 0;
+        for (i = 0; i < items.length(); i++) {
+            letter = items.substring(i, i + 1);
+            if (letter.equals(" ")) {
+                itemToPrint.append(items, indexPrevSpace, i).append("\n");
+                indexPrevSpace = i;
+            }
+        }
+        return ("------------------Receipt------------------" + "\n" +
+        "Total Bill before tip: " + totalBillBeforeTip + "\n" +
+        "Total Percentage: " + tipPercentage + "%" + "\n" +
+        "Total Tip: " + tipAmount() + "\n" +
+        "Total bill with tip: " + totalBill() + "\n" +
+        "Per person cost before tip: " + perPersonCostBeforeTip() + "\n" +
+        "Tip per person: " + perPersonTipAmount() + "\n" +
+        "Per person cost after tip " + perPersonTotalCost() + "\n" +
+        "-------------------------------------------" + "\n" +
+        itemToPrint);
+    }
 }
